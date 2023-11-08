9ab02730751ab4fb7004ef3c8fb43f6007c14933
M0N0G0N <thibauddandoy3d@gmail.com>
2023-11-08 11:47:03 +0100

    Errors Handled

--- kata_1.py
+++ kata_1.py
@@ -2,11 +2,6 @@ def kata_1(seq):
     result = read_rpn(seq.split(' '))
     return result
 
-def kata_input():
-    seq = input("input your RPN code here: ")
-    seq = seq.split(' ')
-    kata_1(seq)
-
 def read_rpn(seq):
     stack = []
     for i in seq:
@@ -16,10 +11,16 @@ def read_rpn(seq):
         except:
             if (i == "+") or (i == "-") or (i == "*") or (i == "/"):
                 stack = basic_op(stack, i)
+            elif (i == "max" or i == "min"):
+                stack = minmax(stack, i)
+            else:
+                raise Exception("Erreur: cet opérande est inconnu!")
     return stack[0]
 
             
 def basic_op(stack, op):
+    if len(stack) <= 1:
+        raise Exception("Erreur: vous avez besoin de plus d'un chiffre pour cette opération!")
     for i in range(1, len(stack)):
         if op == "+":
             stack[0] = stack[0] + stack[i]
@@ -28,8 +29,15 @@ def basic_op(stack, op):
         elif op == "*":
             stack[0] = stack[0] * stack[i]
         elif op == "/":
-            if stack[i] != 0:
-                stack[0] = stack[0] / stack[i]
+            if stack[i] == 0:
+                raise Exception("Erreur: division par 0 temptée!")
+            stack[0] = stack[0] / stack[i]
+            
     return stack[:1]
 
-kata_1("20 20 20 +")
+def minmax(stack, op):
+    if op == "max":
+        stack[0] = max(stack)
+    elif op == "min":
+        stack[0] = min(stack)
+    return stack[:1]
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from kata_1 import kata_1
+import pytest
 
 def test_plus():
     result = kata_1("20 20 20 +")
@@ -19,3 +20,23 @@ def test_division():
 def test_all_op():
     result = kata_1("4 2 + 3 - 2 * 6 /")
     assert result == 1
+    
+def test_min():
+    result = kata_1("20 6 627 1 min")
+    assert result == 1
+
+def test_max():
+    result = kata_1("20 6 627 1 max")
+    assert result == 627
+
+def test_0_division():
+    with pytest.raises(Exception, match="Erreur: division par 0 temptée!"):
+        kata_1("1 0 /")
+        
+def test_small_stack():
+    with pytest.raises(Exception, match="Erreur: vous avez besoin de plus d'un chiffre pour cette opération!"):
+        kata_1("2 +")
+        
+def test_unknown_op():
+    with pytest.raises(Exception, match="Erreur: cet opérande est inconnu!"):
+        kata_1("2 5 + m")
