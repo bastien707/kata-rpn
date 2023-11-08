e5c13aa141c238011247fa2bb8f98bdec3f5cd35
M0N0G0N <thibauddandoy3d@gmail.com>
2023-11-08 12:11:22 +0100

    Removed redundancy

--- kata_1.py
+++ kata_1.py
@@ -22,17 +22,9 @@ def basic_op(stack, op):
     if len(stack) <= 1:
         raise Exception("Erreur: vous avez besoin de plus d'un chiffre pour cette opération!")
     for i in range(1, len(stack)):
-        if op == "+":
-            stack[0] = stack[0] + stack[i]
-        elif op == "-":
-            stack[0] = stack[0] - stack[i]
-        elif op == "*":
-            stack[0] = stack[0] * stack[i]
-        elif op == "/":
-            if stack[i] == 0:
-                raise Exception("Erreur: division par 0 temptée!")
-            stack[0] = stack[0] / stack[i]
-            
+        if op == "/" and stack[i] == 0:
+            raise Exception("Erreur: division par 0 temptée!")
+        stack[0] = eval('stack[0] ' + op + ' stack[i]')
     return stack[:1]
 
 def minmax(stack, op):
